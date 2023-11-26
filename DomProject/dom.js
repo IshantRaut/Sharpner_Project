9084b74280e4bf600306ee44ea03f00372fa6ddd
IshantRaut <ishjee2018@gmail.com>
2023-11-26 15:14:56 +0530

    Third  Commit

--- DomProject/dom.js
+++ DomProject/dom.js
@@ -34,10 +34,40 @@ bold.style.fontWeight='900';
 //Get element by classname
 var items= document.getElementsByClassName('list-group-item');
 console.log(items);
-items[1].textContent =' Hello 2';
+// items[1].textContent =' Hello 2';
 items[2].style.fontWeight='bold';
 items[2].style.backgroundColor='green';
 
 for(var i =0;i< items.length;i++){
     items[i].style.fontWeight='bold';
 }
+
+
+//Get Element by TagName
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+
+for(var i=0;i< li.length;i++){
+    li[i].style.fontWeight='bold';
+}
+
+//Query Selector
+var header = document.querySelector('#main-header')
+header.style.borderBottom='solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value='Hello World';
+
+var submit = document.querySelector('input[type ="submit"]');
+submit.value= 'SEND';
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';
